58d3c75028b8b5275afbb9d1645e26303b65edf0
tjwnstlr3459 <70847393+tjwnstlr3459@users.noreply.github.com>
2021-03-26 21:59:42 +0900

    멤버DB와 보드DB의 데이터값을 오고가는 값의 변수들 저장

---
+++ Community/src/model/vo/Board.java
@@ -0,0 +1,63 @@
+package model.vo;
+
+import java.sql.Date;
+
+public class Board {
+
+	private int 	boardNo;
+	private String	boardTitle;
+	private String 	boardContent;
+	private String	boardWriter;
+	private int		count;
+	private	Date	WDate;
+	public Board() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Board(int boardNo, String boardTitle, String boardContent, String boardWriter, int count, Date wDate) {
+		super();
+		this.boardNo = boardNo;
+		this.boardTitle = boardTitle;
+		this.boardContent = boardContent;
+		this.boardWriter = boardWriter;
+		this.count = count;
+		WDate = wDate;
+	}
+	public int getBoardNo() {
+		return boardNo;
+	}
+	public void setBoardNo(int boardNo) {
+		this.boardNo = boardNo;
+	}
+	public String getBoardTitle() {
+		return boardTitle;
+	}
+	public void setBoardTitle(String boardTitle) {
+		this.boardTitle = boardTitle;
+	}
+	public String getBoardContent() {
+		return boardContent;
+	}
+	public void setBoardContent(String boardContent) {
+		this.boardContent = boardContent;
+	}
+	public String getBoardWriter() {
+		return boardWriter;
+	}
+	public void setBoardWriter(String boardWriter) {
+		this.boardWriter = boardWriter;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public Date getWDate() {
+		return WDate;
+	}
+	public void setWDate(Date wDate) {
+		WDate = wDate;
+	}
+	
+}
---
+++ Community/src/model/vo/Member.java
@@ -0,0 +1,54 @@
+package model.vo;
+
+public class Member {
+
+	private int 	memberNo;
+	private String 	memberId;
+	private String 	memberPw;
+	private String 	memberName;
+	private int		phone;
+	public Member() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Member(int memberNo, String memberId, String memberPw, String memberName, int phone) {
+		super();
+		this.memberNo = memberNo;
+		this.memberId = memberId;
+		this.memberPw = memberPw;
+		this.memberName = memberName;
+		this.phone = phone;
+	}
+	public int getMemberNo() {
+		return memberNo;
+	}
+	public void setMemberNo(int memberNo) {
+		this.memberNo = memberNo;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getMemberPw() {
+		return memberPw;
+	}
+	public void setMemberPw(String memberPw) {
+		this.memberPw = memberPw;
+	}
+	public String getMemberName() {
+		return memberName;
+	}
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	
+	
+}
